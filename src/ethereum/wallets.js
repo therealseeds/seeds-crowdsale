1e3a0efb19681538d777db56dffda60aff1e5aca
Seeds <rachel@playseeds.com>
2018-02-17 15:02:23 -0500

    Update wallets.js

--- src/ethereum/wallets.js
+++ src/ethereum/wallets.js
@@ -42,8 +42,8 @@ export const withdrawFromWallet = (index, address) => {
   const wallet = getWallet(index);
   const privateKey = wallet.getPrivateKey();
 
-  const gasLimit = 21000;
-  const gasPrice = web3.toWei(140, "gwei");
+  const gasLimit = 75000;
+  const gasPrice = web3.toWei(100, "gwei");
   const value = balance - (gasLimit * gasPrice);
 
   const rawTx = {
